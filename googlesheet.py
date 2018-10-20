30baf6e985db36fea17ba093b24bc215d68263c4
ZhaoGuan <guanzhaogz@gmail.com>
2018-10-20 13:56:02 +0800

    path

--- googlesheet.py
+++ googlesheet.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 # __author__ = 'Gz'
-import pyaml
 import requests
 import json
-import time
 import os
 
 """
@@ -14,26 +12,28 @@ https://www.googleapis.com/auth/drive.readonly	允许以只读方式访问用户
 https://www.googleapis.com/auth/drive.file	对文件创建或打开的文件的每文件访问权限。
 https://www.googleapis.com/auth/drive	访问所有用户文件的完整，允许的范围。仅在严格必要时才请求此范围。
 """
+PATH = os.path.dirname(os.path.abspath(__file__))
+PATH_client_secret = PATH + "/client_secret.json"
 
 
 def token_config(data):
-    with open('./token_config.json', 'w') as config:
+    with open(PATH + '/token_config.json', 'w') as config:
         json.dump(data, config)
 
 
 def get_token_config():
-    with open('./token_config.json') as config:
+    with open(PATH + '/token_config.json') as config:
         result = json.load(config)
         return result
 
 
 def access_token(data):
-    with open('./access_token_config.json', 'w') as config:
+    with open(PATH + '/access_token_config.json', 'w') as config:
         json.dump(data, config)
 
 
 def get_access_token_config():
-    with open('./access_token_config.json') as config:
+    with open(PATH + '/access_token_config.json') as config:
         result = json.load(config)['access_token']
         return result
 
@@ -44,19 +44,19 @@ class GoogleOAuth2:
         self.client_id = ""
         self.client_secret = ""
         self.auth_uri = ""
-        self.token_uri = ""
+        self.token_uri = "https://www.googleapis.com/oauth2/v4/token"
         self.redirect_uri = ""
 
-    def get_config(self, config_path="./client_secret.json"):
+    def get_config(self, config_path=PATH_client_secret):
         with open(config_path) as config:
             config = json.load(config)
             self.client_id = config["installed"]["client_id"]
             self.client_secret = config["installed"]["client_secret"]
             self.auth_uri = config["installed"]["auth_uri"]
-            self.token_uri = config["installed"]["token_uri"]
+            # self.token_uri = config["installed"]["token_uri"]
             self.redirect_uri = config["installed"]["redirect_uris"]
 
-    def get_code(self, scope, config_path="./client_secret.json"):
+    def get_code(self, scope, config_path=PATH_client_secret):
         self.get_config(config_path)
         url = "https://accounts.google.com/o/oauth2/auth?" \
               "scope=%s&" \
@@ -69,10 +69,11 @@ class GoogleOAuth2:
         print("请点击URL,为客户端授权,并获取跳转后URL的code值")
         return input('请输入code值:')
 
-    def first_get_token(self, scope, config_path="./client_secret.json"):
+    def first_get_token(self, scope, config_path=PATH_client_secret):
         self.get_config(config_path)
         code = self.get_code(scope)
         url = self.token_uri
+        print(url)
         header = {'Host': 'www.googleapis.com', 'Content-Type': 'application/x-www-form-urlencoded'}
         data = {'code': code,
                 'client_id': self.client_id,
@@ -85,7 +86,7 @@ class GoogleOAuth2:
         token_config(json.loads(response.text))
         access_token(json.loads(response.text))
 
-    def refresh_token(self, config_path="./client_secret.json"):
+    def refresh_token(self, config_path=PATH_client_secret):
         self.get_config(config_path)
         url = self.token_uri
         refresh_token = get_token_config()['refresh_token']
@@ -99,8 +100,8 @@ class GoogleOAuth2:
         response = requests.post(url=url, data=data, headers=header)
         access_token(json.loads(response.text))
 
-    def get_access_token(self, scope, config_path="./client_secret.json"):
-        if os.path.exists('./token_config.json') is False:
+    def get_access_token(self, scope, config_path=PATH_client_secret):
+        if os.path.exists(PATH + '/token_config.json') is False:
             self.first_get_token(scope, config_path)
         else:
             self.refresh_token(config_path)
@@ -122,6 +123,6 @@ class GoogleSheet:
 
 
 if __name__ == '__main__':
-    GS = GoogleSheet('https://www.googleapis.com/auth/spreadsheets', '1zdCyYDe8I--faELjPcc7K45QPoy9iS8SZa02vb-au_4')
+    GS = GoogleSheet('https://www.googleapis.com/auth/spreadsheets', '1iQxXafRnRqMepFaRRSWchQZYsHS5NY92gcCeD7pvTew')
     sheet = GS.read_sheet('工作表1', 'A1', 'G')
     print(sheet)
