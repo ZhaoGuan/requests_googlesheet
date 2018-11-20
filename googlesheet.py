20503a171295691b93c709bb15ff3d136a65d685
ZhaoGuan <guanzhaogz@gmail.com>
2018-11-20 16:52:34 +0800

    save

--- googlesheet.py
+++ googlesheet.py
@@ -118,7 +118,7 @@ class GoogleSheet:
 
     def get_sheet(self, sheet_no):
         get_sheet_url = "https://sheets.googleapis.com/v4/spreadsheets/{}/values/!".format(self.sheet_id, sheet_no)
-        response = requests.post(get_sheet_url, headers=self.sheet_header)
+        response = requests.get(get_sheet_url, headers=self.sheet_header)
         return response.text
 
     def read_sheet(self, sheet_no, begin_cell, end_cell):
@@ -152,6 +152,6 @@ class GoogleSheet:
 if __name__ == '__main__':
     GS = GoogleSheet('https://www.googleapis.com/auth/spreadsheets', '1OnABAiAUjIQTDlkJZnzLx2eybzRviJ5kEzJQic-oTdE')
     # sheet = GS.read_sheet('popular', 'A1', '')
-    sheet = GS.get_sheet()
+    sheet = GS.get_sheet("popular")
     # sheet = GS.add_columns(0, 1)
     print(sheet)
